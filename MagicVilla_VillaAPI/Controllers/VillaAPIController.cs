854e77b11d2b04e66fe2215aa647dad09f174643
salallegra <salvatore.allegra@smoothstack.com>
2022-09-13 04:36:54 -0700

    Refactor controller to use ActionResult return type and ok status code

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,16 +11,16 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
         [HttpGet("id")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList
-                .FirstOrDefault(u=>u.Id==id);
+            return Ok(VillaStore.villaList
+                .FirstOrDefault(u=>u.Id==id));
         }
     }
 }
