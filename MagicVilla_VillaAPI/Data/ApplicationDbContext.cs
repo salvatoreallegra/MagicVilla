463fd50b46fc1a6c4c8f172b0ccda298928df5c6
salallegra <salvatore.allegra@smoothstack.com>
2022-10-11 01:16:34 -0700

    Add entity framework, create migration and update database

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Villa> villas { get; set; }
+
+    }
+}
