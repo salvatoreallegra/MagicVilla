4c30d2cb7fa9d01ac2ec0907ed63e5379e9336e1
salallegra <salvatore.allegra@smoothstack.com>
2022-10-10 23:40:54 -0700

    Update villa model and villa dto

--- MagicVilla_VillaAPI/Model/DTO/VillaDTO.cs
+++ MagicVilla_VillaAPI/Model/DTO/VillaDTO.cs
@@ -7,11 +7,17 @@ namespace MagicVilla_VillaAPI.Model.DTO
         public int Id { get; set; }
         [Required]
         [MaxLength(30)]
-        public string? Name { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+
+        public string Amenity { get; set; }
+        public string ImageUrl { get; set; }
 
         public int Sqft { get; set; }
 
         public int Occupancy { get; set; }
-               
+
     }
 }
